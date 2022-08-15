f0553861eeabbd7a0ff85d3d4dbe02bf0ae0e756
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-08-15 15:40:22 +0900

    Basic math and operations

---
+++ variables/math.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Variables</title>
+</head>
+<body>
+<?php
+
+echo 56 + 45;
+echo "<br>";
+echo 56 - 45;
+echo "<br>";
+echo 56 * 45;
+echo "<br>";
+echo 56 / 45;
+echo "<br>";
+echo "<br>";
+
+// you can use the four basic operations and the operation order happens the same as basic math.
+
+$number = 12;
+$number2 = 24;
+
+echo $number + $number1;
+// Of course, you can use math operations with variables.
+
+?>
+
+</body>
+</html>

b4ca808cd6267a9b3dde3b4f219c781674931772
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-08-15 16:31:05 +0900

    if statements

---
+++ data/control_structures/if_statements.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Variables</title>
+</head>
+
+<body>
+    <?php
+
+    if(3 > 10) {
+        echo "three is more than ten";
+    } elseif (4 < 3) {
+        echo "for is less then three";
+    } else {
+        echo "nothing was true";
+    }
+
+
+
+    ?>
+
+</body>
+
+</html>

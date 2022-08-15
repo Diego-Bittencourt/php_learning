e8dc73838cfaa45affc436ff3cc965a8ee247413
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-08-15 16:39:34 +0900

    Comparison and logical operators

---
+++ data/control_structures/comparison_logical.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Variables</title>
+</head>
+
+<body>
+    <?php
+    // Comparison Operators
+
+    // equal ==
+    // identical ===
+    // compare > < >= <=
+    // not equal !=
+    // not identical !=
+
+    // Logical Operators
+
+    // and &&
+    // or ||
+    // not !
+
+    if(4 == 4) {
+        echo "it is equal";
+    }
+
+    if(4 === "4" || 5 < 10) {
+        echo "first condition is false, but second is true";
+    }
+
+    ?>
+
+</body>
+
+</html>

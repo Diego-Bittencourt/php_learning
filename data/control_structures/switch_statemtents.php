6d8bad95b0633f590dbd5250b64f45193ee6bfe0
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-08-15 16:47:50 +0900

    switch statements

---
+++ data/control_structures/switch_statemtents.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Variables</title>
+</head>
+
+<body>
+    <?php
+
+    $number = 10;
+
+    switch($number) {
+
+        case 34:
+        echo "it is 34";
+        break;
+        case 35:
+        echo "is it 35";
+        break;
+        default :
+        echo "We couldn't find anything";
+        break;
+
+    }
+
+
+    ?>
+
+</body>
+
+</html>

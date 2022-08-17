b6073936b8f4f7ef4bf2e01f7fd7379f1c29677c
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-08-18 01:22:21 +0900

    using the global keyword to access global variables inside function

--- function/functions.php
+++ function/functions.php
@@ -24,11 +24,44 @@
 
     function add($number1, $number2) {
         $sum = $number1 + $number2;
-        echo $sum;
+        return $sum;
     }
 
-    add(4, 50);
- 
+    
+echo add(4, 50);
+
+echo "<br>";
+
+$result = add(22, 50);
+$result = add(100, $result);
+
+echo $result;
+
+
+echo "<br>";
+
+$x = "outsite";
+echo "The variable when assigned = " . $x;
+echo "<br>";
+function convert() {
+	$x = "inside";
+};
+convert();
+echo "Without using the glocal keyword = " . $x ;
+
+echo "<br>";
+
+function trueConvert() {
+	global $x;
+    // THe global keyword can be used inside a function and allows the function to access the global variable.
+    //Probably allows to use the pointer insted of a copied variable.
+	$x = "inside";
+
+};
+trueConvert();
+echo "Using the global keyword = " . $x;
+
+
  ?> 
 
  </body>

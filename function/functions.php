806e1b1e70c3c51b360ff2e244339869a8735cd3
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-08-18 00:47:40 +0900

    function syntax

---
+++ function/functions.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+
+ <?php 
+ 
+    function saySomething() {
+        echo "Hello Server";
+    };
+
+    saySomething();
+ 
+ ?> 
+
+ </body>
+</html>

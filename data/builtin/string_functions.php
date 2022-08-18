c09f0c72bc40709a36f3a5a319eb75be56fd2dd2
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-08-18 22:37:29 +0900

    string functions

---
+++ data/builtin/string_functions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$string = "Hello Server.";
+
+echo strlen($string);
+echo "<br>";
+// the strlen function returns the length of a string.
+
+echo strtoupper($string);
+echo "<br>";
+echo strtolower($string);
+echo "<br>";
+//the two functions above returns the string with upper case and lower case,　respectivelly.
+?>
+
+</body>
+</html>

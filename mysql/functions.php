a7ce27fe9276bafc774ec4cf09bb02ff74cb33fc
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-08-21 00:31:57 +0900

    Refactor the code
    I migrated the code related to the updating to the functions.php

--- mysql/functions.php
+++ mysql/functions.php
@@ -21,4 +21,26 @@ while ($row = mysqli_fetch_assoc($result)) {
 }
 
 
+function updateTable() {
+    global $connection;
+$username =  $_POST['username'];
+$password =  $_POST['password'];
+$id = $_POST['id'];
+
+
+//VERY IMPORT, these strings are going to be a sql query, so don't forget to put spaces and commas around words.
+$query = "UPDATE users SET ";
+$query .= "username = '$username', ";
+$query .= "password = '$password' ";
+$query .= "WHERE id = $id ";
+//    The two username and password are inside brackets cuz they are strings. The id is a interger, so quotes are not used.
+//    Instead of writing a long string, I made small strings and concatenate them.
+
+
+$result = mysqli_query($connection, $query);
+if (!$result) {
+ die("Query Failed" . mysqli_error($connection));
+}
+}
+
 ?>

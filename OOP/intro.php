25d0380ff7945519fc35bbf8a67bfeb7b53604c1
Diego Bittencour Mendes <mendesdiego90@gmail.com>
2022-08-26 23:27:02 +0900

    OOP - define a class and class_exists() built in function

---
+++ OOP/intro.php
@@ -0,0 +1,30 @@
+<?php 
+
+//Defining a class
+class Car {
+
+}
+//first letter is capitalized
+
+if (class_exists("Car")) {
+    //the class_exists() is a built in function that checks the existence of a class.
+echo "yayyyy nice";
+} else {
+    echo "NooooooOO";
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
